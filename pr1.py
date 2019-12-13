d214557b34db42d535d13b5bfafc92966c19a15b
Nikeliza <nikeliza@mail.ru>
2019-12-13 18:45:37 +0300

    Update pr1.py

--- pr1.py
+++ pr1.py
@@ -205,7 +205,7 @@ return images
 def sizeof_fmt(num, suffix='B'):
     ''' Credit : http://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size '''
     for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
-        if abs(num) < 1024.0:    cg.set_cpu_limit(50)  # TODO : get these as command line options
+        if abs(num) < 1024.0:    cg.set_cpu_limit(50)  # TODO : get these as command line option
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
