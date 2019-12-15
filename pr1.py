6ee4d78813625f70b950d92c5cb215329440fcd6
Nikeliza <nikeliza@mail.ru>
2019-12-15 15:08:25 +0300

    Update pr1.py

--- pr1.py
+++ pr1.py
@@ -249,7 +249,8 @@ def mocker_run(uuid1, *args):
 
     btrfsutil.create_snapshot(btrfs_path + '/' + uuid1, btrfs_path + '/' + uuid_name)
     file = open(btrfs_path + '/' + uuid_name + '/' + uuid_name + '.cmd', 'w')
-    file.write(str(cmd))
+    for i in cmd:
+        file.write(str(i))
     file.close()
     cg = Cgroup(uuid_name)
     cg.set_cpu_limit(50)  # TODO : get these as command line options
