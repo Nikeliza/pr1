2b1e8f90505af3d6e9c2e3aaadeb1cd82f467db2
Nikeliza <nikeliza@mail.ru>
2019-12-15 14:45:14 +0300

    Update pr1.py

--- pr1.py
+++ pr1.py
@@ -339,7 +339,7 @@ def mocker_help():
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 1:
+    if len(sys.argv) < 2:
         print('Do not enter command')
     elif sys.argv[1] == 'help':
         mocker_help()
